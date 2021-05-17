1e483879e612746f5a028b94ffb479679330ae71
Artyom Gurbovich <artyom.gurbovich@gmail.com>
2021-05-17 20:52:50 +0300

    feat: implement exercise 2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,15 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int result = 0;
+    for (int i = 0; i < [array count]; i++) {
+        for (int j = 0; j < [array count]; j++) {
+            if (i != j && [NSNumber numberWithInt:([[array objectAtIndex:(NSInteger)i] intValue] - [[array objectAtIndex:(NSInteger)j] intValue])] == number) {
+                result += 1;
+            }
+        }
+    }
+    return (NSInteger) result;
 }
 
 @end
