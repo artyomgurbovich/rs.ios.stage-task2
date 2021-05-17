770baba4ab603996c5b7d39b2c307accc360681f
Artyom Gurbovich <artyom.gurbovich@gmail.com>
2021-05-17 20:52:21 +0300

    feat: implement exercise 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,17 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSMutableString *str = [NSMutableString stringWithFormat:@""];
+    for(NSInteger numberCopy = n; numberCopy > 0; numberCopy >>= 1) [str insertString:((numberCopy & 1) ? @"1" : @"0") atIndex:0];
+    while([str length] < 8) str = (NSMutableString*)[@"0" stringByAppendingString: str];
+    NSMutableString *reversedString = [NSMutableString string];
+    NSInteger charIndex = [str length];
+    while (charIndex > 0) {
+        charIndex--;
+        NSRange subStrRange = NSMakeRange(charIndex, 1);
+        [reversedString appendString:[str substringWithRange:subStrRange]];
+    }
+    const char* utf8String = [reversedString UTF8String];
+    char* endPtr = NULL;
+    return strtol(utf8String, &endPtr, 2);
 }
